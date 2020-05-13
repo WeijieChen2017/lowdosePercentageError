b24d86240b864a63e715d2fc4f6ab108491d5a13
WeijieChen2017 <weijiechen1994@gmail.com>
2020-05-12 23:45:34 -0500

    v1

--- calculatePercentageError.py
+++ calculatePercentageError.py
@@ -1,4 +1,5 @@
 import numpy as np
+import nibabel as nib
 import glob
 
 caseList = ["003", "005", "006", "008", "010",
@@ -6,16 +7,23 @@ caseList = ["003", "005", "006", "008", "010",
             "017", "018", "019", "022", "023",
             "024", "025", "026", "027", "028"]
 
-def getFile(filePath):
-    path = glob.glob(filePath)[0]
-    print(path)
+def getSUV(filePath, segData):
+    niiPath = glob.glob(filePath)[0]
+    niiData = nib.load(niiPath).get_fdata()
+    segValue = np.multiply(niiData, segData)
+    return(np.mean(segValue))
 
 for caseName in caseList:
+    segPath = "../seg/full_"+caseName+"seg_s.nii.gz"
+    segData = nib.load(segPath).get_fdata()
+
     fullPath = "../fulldose/mre"+caseName+"_5min.nii.gz"
     lowPath = "../lowdose/mre"+caseName+"_1min.nii.gz"
     petPath = "../PET-only/PET-ONLY_*_mre"+caseName+"*.nii.gz"
     mrPath = "../MR-only/MRI-ONLY_*_mre"+caseName+"*.nii.gz"
     mixPath = "../PET-MR/PET-MRI_*_mre"+caseName+"*.nii.gz"
 
+    print(caseName, " ",end="")
     for path in [fullPath, lowPath, petPath, mrPath, mixPath]:
-        getFile(path)
+        print(getSUV(path), end="")
+    print("")
